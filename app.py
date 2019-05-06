78b559f16aee8173257160465e4423473dbfcdb0
Emre Akoz <akozemr@gmail.com>
2019-05-05 23:28:27 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+from emotion_predictor import emotion_predictor
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('/home.html')
+    else:
+        im_file = request.files["myfile"]
+        im_file.save(secure_filename(im_file.filename))
+        #path = '/Users/emre/apps/capstone_app/old_images/' + im_file.filename
+        printout1, printout2 = emotion_predictor()
+        
+        return render_template('/project.html', result1=printout1, 
+                               result2 = printout2, result3 = im_file.filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ emotion_predictor.py
@@ -0,0 +1,65 @@
+from network import network
+from PIL import Image
+import numpy as np
+import os
+import shutil
+import glob
+
+def image_filtering(image_name):
+    height, width = 128, 128
+    im = np.empty(shape=(1,128,128,3))
+    img = Image.open(image_name)
+    img = img.resize((height, width), Image.ANTIALIAS)
+    im[0,:,:,:] = np.array(img)[:,:,:3]
+    
+    return im
+
+def move_image(image_path, root):
+    shutil.copy(image_path, root+'static')
+    os.remove(image_path)
+    pass
+
+def retrieve_image():
+#    root = '/app'
+    root = '/Users/emre/apps/capstone_app/'
+    
+    if glob.glob(root+'*.jpg'):
+        image_path = glob.glob(root+'*.jpg')[0]
+    elif glob.glob(root+'*.png'):
+        image_path = glob.glob(root+'*.png')[0]
+    elif glob.glob(root+'*.jpeg'):
+        image_path = glob.glob(root+'*.jpeg')[0]
+    elif glob.glob(root+'*.eps'):
+        image_path = glob.glob(root+'*.eps')[0]
+    
+    return root, image_path
+       
+def emotion_predictor():
+    root, image_path = retrieve_image()
+    im = image_filtering(image_path)
+    
+    model = network()
+    model.load_weights('./model_weights.h5') #load weights
+    probs = list(model.predict(im)[0])
+    emotion_probs = probs.copy()
+    move_image(image_path, root)
+    
+    emotions_dict = {0:'neutral', 1:'happy', 2:'sad', 3:'surprised', 4:'fearful', 
+                     5:'disgusted', 6:'angry', 7:'contempt'}
+    
+    emotions_dict_ = {0:'neutral', 1:'happiness', 2:'sadness', 3:'surprise', 4:'fear', 
+                     5:'disgust', 6:'anger', 7:'contempt'}
+    
+    top1_emotion, probs[probs.index(max(probs))] = probs.index(max(probs)), 0
+    top2_emotion, probs[probs.index(max(probs))] = probs.index(max(probs)), 0
+    top3_emotion = probs.index(max(probs))
+    
+    printout1 = 'You look {} in this picture!'.format(
+            emotions_dict[top1_emotion])
+    printout2 = 'The other two probable emotions are {} and {}.'.format(
+            emotions_dict_[top2_emotion], emotions_dict_[top3_emotion])
+    
+    return printout1, printout2
+
+if __name__ == '__main__':
+    result = emotion_predictor()
